ccc2096d813b650805ebf8086f9fbfe7fdd838cd
Hassan ESSADIK <hassanessadtri@gmail.com>
2022-01-09 13:14:36 +0100

    in process

---
+++ admin_menu.php
@@ -0,0 +1,41 @@
+<ul>
+  
+  <li>
+    <a href="categorie.php" >
+      <i class="glyphicon glyphicon-indent-left"></i>
+      <span>Catégories</span>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-th-large"></i>
+      <span>Produits</span>
+    </a>
+    <ul class="nav submenu">
+       <li><a href="product.php">Gestion des produits</a> </li>
+       <li><a href="add_product.php">Ajouter des produits</a> </li>
+   </ul>
+  </li>
+
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-th-list"></i>
+       <span>Ventes</span>
+      </a>
+      <ul class="nav submenu">
+         <li><a href="sales.php">Gestion de ventes</a> </li>
+         <li><a href="add_sale.php">Ajouter des ventes</a> </li>
+     </ul>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-signal"></i>
+       <span>Rapport des ventes</span>
+      </a>
+      <ul class="nav submenu">
+        <li><a href="sales_report.php">Ventes par dates</a></li>
+        <li><a href="monthly_sales.php">Ventes mensuelles</a></li>
+        <li><a href="daily_sales.php">ventes quotidiennes</a> </li>
+      </ul>
+  </li>
+</ul>
---
+++ home.php
@@ -0,0 +1,22 @@
+<?php
+$page_title = 'Home Page';
+require_once('includes/load.php');
+if (!$session->isUserLoggedIn(true)) {
+  redirect('index.php', false);
+}
+?>
+<?php include_once('header.php'); ?>
+<div class="row">
+  <div class="col-md-12">
+    <?php echo display_msg($msg); ?>
+  </div>
+  <div class="col-md-12">
+    <div class="panel">
+      <div class="jumbotron text-center">
+        <h1>Gestion de stock</h1>
+        <img src="images/tableau-dior-logos-arc-en-ciel-decoratifs.jpg" style="width: 90%" alt="" srcset="">
+      </div>
+    </div>
+  </div>
+</div>
+<?php include_once('footer.php'); ?>
