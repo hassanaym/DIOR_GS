42f39f8c1baa7f233b5c24c857b4c1a9a4c135b9
Hassan ESSADIK <hassanessadtri@gmail.com>
2022-01-09 13:27:36 +0100

    in process 2

--- admin_menu.php
+++ admin_menu.php
@@ -1,7 +1,7 @@
 <ul>
-  
+
   <li>
-    <a href="categorie.php" >
+    <a href="categorie.php">
       <i class="glyphicon glyphicon-indent-left"></i>
       <span>Catégories</span>
     </a>
@@ -12,30 +12,53 @@
       <span>Produits</span>
     </a>
     <ul class="nav submenu">
-       <li><a href="product.php">Gestion des produits</a> </li>
-       <li><a href="add_product.php">Ajouter des produits</a> </li>
-   </ul>
+      <li><a href="product.php">Gestion des produits</a> </li>
+      <li><a href="add_product.php">Ajouter des produits</a> </li>
+    </ul>
   </li>
 
   <li>
     <a href="#" class="submenu-toggle">
       <i class="glyphicon glyphicon-th-list"></i>
-       <span>Ventes</span>
-      </a>
-      <ul class="nav submenu">
-         <li><a href="sales.php">Gestion de ventes</a> </li>
-         <li><a href="add_sale.php">Ajouter des ventes</a> </li>
-     </ul>
+      <span>Clients</span>
+    </a>
+    <ul class="nav submenu">
+      <li><a href="clients.php">Gestion des clients</a> </li>
+      <li><a href="add_client.php">Ajouter un client</a> </li>
+    </ul>
   </li>
+
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-th-list"></i>
+      <span>Commandes</span>
+    </a>
+    <ul class="nav submenu">
+      <li><a href="sales.php">Gestion des commandes</a> </li>
+      <li><a href="add_sale.php">Ajouter une commande</a> </li>
+    </ul>
+  </li>
+
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-th-list"></i>
+      <span>Approvisionnement</span>
+    </a>
+    <ul class="nav submenu">
+      <li><a href="sales.php">Gestion des approvisionnements</a> </li>
+      <li><a href="add_sale.php">Ajouter un approvisionnement</a> </li>
+    </ul>
+  </li>
+
   <li>
     <a href="#" class="submenu-toggle">
       <i class="glyphicon glyphicon-signal"></i>
-       <span>Rapport des ventes</span>
-      </a>
-      <ul class="nav submenu">
-        <li><a href="sales_report.php">Ventes par dates</a></li>
-        <li><a href="monthly_sales.php">Ventes mensuelles</a></li>
-        <li><a href="daily_sales.php">ventes quotidiennes</a> </li>
-      </ul>
+      <span>Rapport des ventes</span>
+    </a>
+    <ul class="nav submenu">
+      <li><a href="sales_report.php">Ventes par dates</a></li>
+      <li><a href="monthly_sales.php">Ventes mensuelles</a></li>
+      <li><a href="daily_sales.php">ventes quotidiennes</a> </li>
+    </ul>
   </li>
 </ul>
